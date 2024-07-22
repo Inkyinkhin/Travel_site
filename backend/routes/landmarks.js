b861c6fe96063ea5ddb0ad4cd83bd951ab68e174
InKyinKhin <inkyinkhin238@gmail.com>
2024-07-22 14:51:06 +0630

    add dummy data

--- backend/routes/landmarks.js
+++ backend/routes/landmarks.js
@@ -3,8 +3,33 @@ const Database = require("better-sqlite3");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const csv = require("csv-parser");
 const router = express.Router();
 const db = new Database("db/landmarks.db");
+
+// insert cities
+
+const csvFilePath = path.join(__dirname, "cities/rakhine.csv");
+const results = [];
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    const stmt = db.prepare(
+      "INSERT INTO cities (name, lad, lung) VALUES (?, ?, ?)"
+    );
+    const insertMany = db.transaction((cities) => {
+      for (const city of cities) {
+        stmt.run(city.City, parseFloat(city.lat), parseFloat(city.long));
+      }
+    });
+    try {
+      insertMany(results);
+      console.log("Data inserted successfully");
+    } catch (error) {
+      console.error("Failed to insert data", error);
+    }
+  });
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
@@ -22,12 +47,12 @@ const upload = multer({ storage: storage });
 
 // Create a new landmark with images
 router.post("/add_landmark", upload.array("images", 10), (req, res) => {
-  console.log("triggered");
-  const { name, location, description } = req.body;
+  const { name, location, description, latitude, longitude } = req.body;
+  console.log(req.body);
   const stmt = db.prepare(
-    "INSERT INTO landmarks (name, location, description) VALUES (?, ?, ?)"
+    "INSERT INTO landmarks (name, location, description, lad, lung ) VALUES (?, ?, ?,?,?)"
   );
-  const info = stmt.run(name, location, description);
+  const info = stmt.run(name, location, description, latitude, longitude);
   const landmarkId = info.lastInsertRowid;
 
   if (req.files) {
@@ -40,6 +65,25 @@ router.post("/add_landmark", upload.array("images", 10), (req, res) => {
   }
 
   res.json({ id: landmarkId });
+});
+
+// Create a new city
+router.get("/get_city/:name", (req, res) => {
+  const { name } = req.body;
+  const stmt = db.prepare("SELECT * FROM cities WHERE name = ?");
+  const info = stmt.run(name);
+  const cityId = info.lastInsertRowid;
+
+  // if (req.files) {
+  //   const imgStmt = db.prepare(
+  //     "INSERT INTO images (city_id, path) VALUES (?, ?)"
+  //   );
+  //   for (const file of req.files) {
+  //     imgStmt.run(landmarkId, file.path);
+  //   }
+  // }
+
+  res.json({ id: cityId });
 });
 
 // Update a landmark
